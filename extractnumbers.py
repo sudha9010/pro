dc0ec42ac8351c83d821146a9e4c2843d4f81589
sudha9010 <132307139+sudha9010@users.noreply.github.com>
2023-12-12 17:37:45 +0000

    commit

--- extractnumbers.py
+++ extractnumbers.py
@@ -1,15 +1,15 @@
 import re
 
 def extract_numbers(input_string):
-    # Use regular expression to find numbers in the input string
+    
     numbers = re.findall(r'\d+', input_string)
     
-    # Convert the list of strings to integers
+    
     numbers = [int(num) for num in numbers]
     
     return numbers
 
-# Example usage:
+
 input_string = "Hello123World456"
 result = extract_numbers(input_string)
 
